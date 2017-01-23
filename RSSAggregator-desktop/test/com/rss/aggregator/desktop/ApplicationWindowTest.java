8a8460848a470ad56bf4c1c970c3d5c316f977c9
Arkayl <pierre.hennebois@epitech.eu>
2017-01-23 19:47:33 +0100

    debut de tests

---
+++ RSSAggregator-desktop/test/com/rss/aggregator/desktop/ApplicationWindowTest.java
@@ -0,0 +1,27 @@
+package com.rss.aggregator.desktop;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApplicationWindowTest {
+	protected ApplicationWindow appWin;
+	
+	@Before
+	public void setUp() throws Exception {
+		appWin = new ApplicationWindow();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+//		assert;
+		fail("Not yet implemented");
+	}
+
+}
