3aa7f82f245ffc3f0a380009bf5b8c8c6ff892e8
Ilyes Abd-Lillah <ilyes.abd-lillah@epitech.eu>
2017-01-29 19:23:28 +0100

    Adding last features

---
+++ RSSAggregator-android/app/src/main/java/com/abd_lillah/ilyes/rssaggregator/SignUPActivity.java
@@ -0,0 +1,7 @@
+package com.abd_lillah.ilyes.rssaggregator;
+
+/**
+ * Created by ilyes on 29/01/2017.
+ */
+public class SignUPActivity {
+}
---
+++ RSSAggregator-android/app/src/main/java/com/abd_lillah/ilyes/rssaggregator/UserListAdapter.java
@@ -0,0 +1,68 @@
+package com.abd_lillah.ilyes.rssaggregator;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by ilyes on 28/01/2017.
+ */
+public class UserListAdapter
+        extends RecyclerView.Adapter<UserListAdapter.FeedModelViewHolder> {
+
+    private List<RssFeedModel> mRssFeedModels;
+    private Context mContext;
+    public static class FeedModelViewHolder extends RecyclerView.ViewHolder {
+        private View rssFeedView;
+
+        public FeedModelViewHolder(View v) {
+            super(v);
+            rssFeedView = v;
+        }
+    }
+
+
+    public UserListAdapter(List<RssFeedModel> rssFeedModels) {
+        mRssFeedModels = rssFeedModels;
+    }
+
+    @Override
+    public FeedModelViewHolder onCreateViewHolder(ViewGroup parent, final int type) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_rss_feed, parent, false);
+        FeedModelViewHolder holder = new FeedModelViewHolder(v);
+        return holder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(final FeedModelViewHolder holder, final int position) {
+        final RssFeedModel rssFeedModel = mRssFeedModels.get(position);
+
+        ((TextView)holder.rssFeedView.findViewById(R.id.titleText)).setText(rssFeedModel.title);
+        ((TextView)holder.rssFeedView.findViewById(R.id.descriptionText)).setText(rssFeedModel.description);
+        ((TextView)holder.rssFeedView.findViewById(R.id.linkText)).setText(rssFeedModel.link);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(rssFeedModel.link);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                mContext = view.getContext();
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRssFeedModels.size();
+    }
+}
